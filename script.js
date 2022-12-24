8f0d93e5cb56e9ff9deb6904ce45f0050941781e
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2022-12-24 21:46:08 +0000

    Added a hamburger menu to tappedesigns

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-const hamburger_icon = document.getElementById("hamburger_icon").addEventListener('click', (e) => {
-    
-    
+const hamburger_icon = document.getElementById("hamburger_icon");
+const hamburger_menu = document.getElementById("hamburger_menu");
 
+let clickedBefore = false;
+
+hamburger_icon.addEventListener('click', (e) => {
+
+    const open = () => {
+        document.getElementById("hamburger_menu").style.display = 'flex';
+        clickedBefore = true;
+    }
+
+    const close = () => {
+        document.getElementById("hamburger_menu").style.display = 'none';
+        clickedBefore = false;
+    }
+
+    clickedBefore == false ? open() : close();
 });
+
+
+
+
+
+// if(toggle == true) {
+//     hamburger_icon.addEventListener('click', (e) => {
+//         document.getElementById("hamburger_menu").style.display = 'none';
+//         toggle = false;
+//         alert(2)
+//     });
+// }
