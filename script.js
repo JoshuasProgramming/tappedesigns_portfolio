e27cf7fa21cf0de40411107d1c275ab5f6f9b8ca
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2022-12-24 22:02:33 +0000

    Added some comments on script.js

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
+//grabbing the hamburger icon and hamburger menu
 const hamburger_icon = document.getElementById("hamburger_icon");
 const hamburger_menu = document.getElementById("hamburger_menu");
 
+//setting a boolean variable to check id the user has clicked on the hamburger icon 
 let clickedBefore = false;
 
+//once the user has clicked on the hamburger icon...
 hamburger_icon.addEventListener('click', (e) => {
 
+    //open the menu
     const open = () => {
         document.getElementById("hamburger_menu").style.display = 'flex';
         clickedBefore = true;
     }
 
+    //close the menu
     const close = () => {
         document.getElementById("hamburger_menu").style.display = 'none';
         clickedBefore = false;
     }
 
+    //set a condition to check whether or not the open or close the menu
     clickedBefore == false ? open() : close();
 });
-
-
-
-
-
-// if(toggle == true) {
-//     hamburger_icon.addEventListener('click', (e) => {
-//         document.getElementById("hamburger_menu").style.display = 'none';
-//         toggle = false;
-//         alert(2)
-//     });
-// }
