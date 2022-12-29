5411434bc4baf5c6e987508ec8a707d3991ad24e
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2022-12-29 00:54:05 +0000

    Added comments & completed a few things

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ let clickedBefore = false;
 //once the user has clicked on the hamburger icon...
 hamburger_icon.addEventListener('click', (e) => {
 
-    //open the menu
+    //open the menu function
     const open = () => {
         document.getElementById("hamburger_menu").style.display = 'flex';
         clickedBefore = true;
     }
 
-    //close the menu
+    //close the menu function
     const close = () => {
         document.getElementById("hamburger_menu").style.display = 'none';
         clickedBefore = false;
     }
 
-    //set a condition to check whether or not the open or close the menu
+    //set a condition to check whether or not to open or close the menu
     clickedBefore == false ? open() : close();
 });
